1d414bbd8f01f531ace12ed42f094804e1274153
philippe Chevalier <admin.chevalier@gmail.com>
2018-07-13 08:36:58 +0200

    Add files via upload

---
+++ F/_sidebar.php
@@ -0,0 +1,7 @@
+<div id="sidebar" >
+			<?php	include('../commande/_commande.php');	?>
+			<?php	include('../cotisation/_add.php');	?>
+			<?php	include('../don/_add.php');	?>
+			<?php	include('../newsletter/_add.php');	?>
+			<?php	include('../contact/_lien.php');	?>
+</div>
