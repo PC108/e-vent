be4739e0b483bedb4d00a9ff0054e071fdb7e694
Hubert Alcin <ha@np6.com>
2018-07-20 15:20:06 +0100

    change connection sql function for compatibility with PHP 7

--- cnx/db_dev.php
+++ cnx/db_dev.php
@@ -1,19 +1,32 @@
 <?php
-
 if ($_SERVER['SERVER_NAME'] == "localhost" || $_SERVER['SERVER_NAME'] == "127.0.0.1") { // en local
-    $hostname = "localhost";
-    $database = "event-database";
+    $hostname = "db";
+    $database = "oneplayecentre";
+    $port =3301;
     $username = "root";
-    $password = "";
+    $password = "root";
 } else { // sur le serveur
     $hostname = "";
     $database = "";
     $username = "";
     $password = "";
 };
+$connexion = mysqli_connect($hostname, $username, $password) or trigger_error(mysql_error(), E_USER_ERROR);
+//mysql_select_db($database, $connexion);
+//mysql_query("SET NAMES 'UTF8' ");
 
-$connexion = mysql_connect($hostname, $username, $password) or trigger_error(mysql_error(), E_USER_ERROR);
+/* Vérification de la connexion */
+if (mysqli_connect_errno()) {
+    printf("Echec de la connexion : %s\n", mysqli_connect_error());
+    exit();
+}
 
-mysql_select_db($database, $connexion);
-mysql_query("SET NAMES 'UTF8' ");
+if (!mysqli_query($connexion, "SET NAMES 'UTF8' ")) {
+    printf("Message d'erreur : %s\n", mysqli_error($connexion));
+}
+
+/* Fermeture de la connexion */
+mysqli_close($connexion);
 ?>
+
+
